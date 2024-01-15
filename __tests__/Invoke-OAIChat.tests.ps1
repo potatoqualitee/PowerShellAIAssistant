c582298731e2f1241815ac5e5d6f052def4b09e6
dfinke <finked@hotmail.com>
2024-01-15 08:06:11 -0500

    Add parameter validation and test for Invoke-OAIChat

--- __tests__/Invoke-OAIChat.tests.ps1
+++ __tests__/Invoke-OAIChat.tests.ps1
@@ -8,4 +8,23 @@ Describe "Invoke-OAIChat" -Tag Invoke-OAIChat {
      
         $actual | Should -Not -BeNullOrEmpty
     }
+
+    It 'should have these parameters' {
+        $actual = Get-Command Invoke-OAIChat -ErrorAction SilentlyContinue
+     
+        $actual | Should -Not -BeNullOrEmpty
+     
+        $actual.Parameters.Keys.Contains('UserInput') | Should -Be $true
+     
+        $actual.Parameters.UserInput.Attributes.ValueFromPipeline | Should -Be $true
+     
+        $actual.Parameters.Keys.Contains('Instructions') | Should -Be $true
+     
+        $actual.Parameters.Instructions.Aliases.Count | Should -Be 0
+     
+        $actual.Parameters.Keys.Contains('model') | Should -Be $true
+     
+        $actual.Parameters.model.Attributes.ValidValues.Count | Should -Be 5
+        $actual.Parameters.model.Attributes.ValidValues | Should -Be @('gpt-4', 'gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 'gpt-4-1106-preview', 'gpt-3.5-turbo-1106')
+    }
 }
