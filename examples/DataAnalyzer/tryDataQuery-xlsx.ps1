2589fb36738ce126a84d768843cef32bc1326e6e
dfinke <finked@hotmail.com>
2024-01-17 20:40:02 -0500

    Fix image retrieval and save in DataAnalyzer script

--- examples/DataAnalyzer/tryDataQuery-xlsx.ps1
+++ examples/DataAnalyzer/tryDataQuery-xlsx.ps1
@@ -8,13 +8,23 @@ $userInput = 'Please read the xlsx file. Need a bar chart for these "Units" sold
 $r = Invoke-DataQuery -UserInput $userInput -FilePath $PSScriptRoot\Sales.xlsx
 
 $messages = Get-OAIMessage -ThreadId $r.Thread.id
-$image = $messages[0].data[0].content[0]
-if ($image) {
-    $fileId = $image.image_file.file_id
 
+$fileId = $messages.data.content.image_file.file_id
+if ($fileId) {
     Get-OAIFileContent -FileId $fileId -OutFile ./result-xlsx.jpg
     Write-Host "Result saved to $PSScriptRoot\result-xlsx.jpg"
 }
 else {
     Write-Error "No image found"
 }
+
+# $image = $messages[0].data[0].content[0]
+# if ($image) {
+#     $fileId = $image.image_file.file_id
+
+#     Get-OAIFileContent -FileId $fileId -OutFile ./result-xlsx.jpg
+#     Write-Host "Result saved to $PSScriptRoot\result-xlsx.jpg"
+# }
+# else {
+#     Write-Error "No image found"
+# }
