4baf1e19cc06cd3aacdb29902db8b4634eb121a8
dfinke <finked@hotmail.com>
2024-01-17 20:29:55 -0500

    Add DataAnalyzer scripts for CSV and XLSX files

---
+++ examples/DataAnalyzer/tryDataQuery-xlsx.ps1
@@ -0,0 +1,20 @@
+# https://www.jimmymills.io/blog/openai-assistants-api/code-interpreter
+
+. $PSScriptRoot\Invoke-DataQuery.ps1
+
+$userInput = 'Please read the xlsx file. Need a bar chart for these "Units" sold by "Region"'
+$userInput = 'Please read the xlsx file. Need a bar chart for these "Units" sold by "State"'
+
+$r = Invoke-DataQuery -UserInput $userInput -FilePath $PSScriptRoot\Sales.xlsx
+
+$messages = Get-OAIMessage -ThreadId $r.Thread.id
+$image = $messages[0].data[0].content[0]
+if ($image) {
+    $fileId = $image.image_file.file_id
+
+    Get-OAIFileContent -FileId $fileId -OutFile ./result-xlsx.jpg
+    Write-Host "Result saved to $PSScriptRoot\result-xlsx.jpg"
+}
+else {
+    Write-Error "No image found"
+}
