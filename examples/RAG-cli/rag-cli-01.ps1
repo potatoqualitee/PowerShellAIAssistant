40df8007b930c3e63f475c1ab8f35500a988bb32
dfinke <finked@hotmail.com>
2024-01-30 17:45:33 -0500

    Add ragAssistant function and askQuestion function

--- examples/RAG-cli/rag-cli-01.ps1
+++ examples/RAG-cli/rag-cli-01.ps1
@@ -1,4 +1,5 @@
 $assistantIdFileName = "$PSScriptRoot\assistantId.txt"
+
 function Get-AssistantId {
     Get-Content $assistantIdFileName    
 }
@@ -11,7 +12,72 @@ function Save-AssistantId {
     $AssistantId | Set-Content $assistantIdFileName
 }
 
-$a = New-OAIAssistant
+function ragAssistant {
+    param(
+        $Question,        
+        # [Parameter(ValueFromPipelineByPropertyName)]
+        # [Alias('FullName')]
+        # $Path,
+        [Switch]$Chat,
+        [Switch]$Clear            
+    )
 
-Save-AssistantId $a.id
-Get-AssistantId
+    begin {
+        $files = @()
+    }
+
+    process {
+        if ($Path) {
+            $files += Invoke-OAIUploadFile $Path        
+        }
+    }
+
+    End {
+
+        $targetAssistantId = Get-AssistantId
+        if ($null -ne $targetAssistantId) {
+            Write-Error "No rag assistant found. Please add files."
+            return
+        }
+        elseif (Test-OAIAssistantId $targetAssistantId -eq $false) {
+            New-RagAssistant $files
+        }
+        # else {
+        #     $timestamp = Get-Date -Format "yyyyMMddHHmmss"
+        #     $assistantParams = @{
+        #         Name         = "rag assistant-$timestamp"
+        #         Instructions = "Please read all, repeat all the uploaded file(s) to answer all questions. Pls summarize your answer in 1-2 sentences."
+        #         Model        = 'gpt-4-turbo-preview' 
+        #         Tools        = Enable-OAIRetrievalTool
+        #         FileIds      = $files.id
+        #     }
+        
+        #     $assistant = New-OAIAssistant @assistantParams            
+        #     Save-AssistantId $assistant.id
+        # }
+
+        if (!$Chat -and $null -ne $Question) {
+            askQuestion $Question $assistant
+        }
+        elseif ($Chat) {
+            ChatREPL $assistant
+        }
+    }
+}
+
+function askQuestion {
+    param(
+        $Question,
+        $Assistant
+    )
+
+    Write-Host "Asking the question: " -NoNewline
+    Write-Host -ForegroundColor Yellow $Question
+
+    $queryResult = New-OAIThreadQuery -UserInput $Question -Assistant $Assistant 
+    $null = Wait-OAIOnRun -Run $queryResult.Run -Thread $queryResult.Thread
+    $messages = Get-OAIMessage -ThreadId $queryResult.Thread.Id
+    $messages.data[0].content.text.value
+}
+
+#ragAssistant -Question "How do I get started?"
