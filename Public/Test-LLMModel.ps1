804a865c3a8a85edec12a1fb6161760f9c98102b
dfinke <finked@hotmail.com>
2024-02-16 20:44:55 -0500

    Add Test-LLMModel function to check model validity

--- Public/Test-LLMModel.ps1
+++ Public/Test-LLMModel.ps1
@@ -1,7 +1,7 @@
-function ValidateAIModel {    
+function Test-LLMModel {    
     $provider = Get-OAIProvider
     if ($provider -eq 'OpenAI') {
-        $modelList = 'a', 'b', 'c'
+        $modelList = 'gpt-4', 'gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 'gpt-4-turbo-preview', 'gpt-4-1106-preview', 'gpt-3.5-turbo-1106'
         if ($_ -in $modelList) {
             $true
         }
@@ -11,14 +11,5 @@ function ValidateAIModel {
     }
     elseif ($provider -eq 'AzureOpenAI') {
         $true
-    }    
-    
-}
-function yyz {
-    param(
-        [ValidateScript({ ValidateAIModel })]
-        [string]$model
-    )
- 
-    'You selected: ' + $model
+    }
 }
