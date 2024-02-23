2a6cccf19cafd5af42de9dbcacfe789d227f4e40
Szeraax <6242511+Szeraax@users.noreply.github.com>
2024-02-22 23:36:54 -0700

    Add workaround for Pode.Web on pwsh 7.4+

--- Public/Show-OAILocalPlayground.ps1
+++ Public/Show-OAILocalPlayground.ps1
@@ -20,6 +20,183 @@ function Show-OAILocalPlayground {
         throw 'PowerShellAIAssistant not found in any module directories. Please add a PSModulePath to the correct location if using a custom one. e.g. $ENV:PSModulePath += "; $pwd"'
     }
 
+    # This is almost like a proxy function since it is directly stolen from Pode.Web and then edited so that
+    # ConvertTo-PodeWebPage works with advanced parameter blocks on pwsh 7.4+
+    # Also has a slight improvement to use ConvertTo-Json for output results.
+    # Likely remove this function as soon as Pode.Web >v0.8.3 is released.
+    function Convertto-PodeWebPage {
+        [CmdletBinding()]
+        param(
+            [Parameter(ValueFromPipeline = $true)]
+            [string[]]
+            $Commands,
+
+            [Parameter()]
+            [string]
+            $Module,
+
+            [switch]
+            $GroupVerbs,
+
+            [Parameter()]
+            [Alias('NoAuth')]
+            [switch]
+            $NoAuthentication
+        )
+
+        # if a module was supplied, import it - then validate the commands
+        if (![string]::IsNullOrWhiteSpace($Module)) {
+            Import-PodeModule -Name $Module
+            Export-PodeModule -Name $Module
+
+            Write-Verbose "Getting exported commands from module"
+            $ModuleCommands = (Get-Module -Name $Module | Sort-Object -Descending | Select-Object -First 1).ExportedCommands.Keys
+
+            # if commands were supplied validate them - otherwise use all exported ones
+            if (Test-PodeIsEmpty $Commands) {
+                Write-Verbose "Using all commands in $($Module) for converting to Pages"
+                $Commands = $ModuleCommands
+            }
+            else {
+                Write-Verbose "Validating supplied commands against module's exported commands"
+                foreach ($cmd in $Commands) {
+                    if ($ModuleCommands -inotcontains $cmd) {
+                        throw "Module $($Module) does not contain function $($cmd) to convert to a Page"
+                    }
+                }
+            }
+        }
+
+        # if there are no commands, fail
+        if (Test-PodeIsEmpty $Commands) {
+            throw 'No commands supplied to convert to Pages'
+        }
+
+        $sysParams = [System.Management.Automation.PSCmdlet]::CommonParameters.GetEnumerator() | ForEach-Object { $_ }
+
+        # create the pages for each of the commands
+        foreach ($cmd in $Commands) {
+            Write-Verbose "Building page for $($cmd)"
+            $cmdInfo = (Get-Command -Name $cmd -ErrorAction Stop)
+
+            $sets = $cmdInfo.ParameterSets
+            if (($null -eq $sets) -or ($sets.Length -eq 0)) {
+                continue
+            }
+
+            # for cmdlets this will be null
+            $ast = $cmdInfo.ScriptBlock.Ast
+            $paramDefs = $null
+            if ($null -ne $ast) {
+                $paramDefs = $ast.FindAll({ $args[0] -is [System.Management.Automation.Language.ParameterAst] }, $true) | Where-Object {
+                    $_.Parent.Parent.Parent.Name -ieq $cmd
+                }
+            }
+
+            $tabs = New-PodeWebTabs -Tabs @(foreach ($set in $sets) {
+                    $elements = @(foreach ($param in $set.Parameters) {
+                            if ($sysParams -icontains $param.Name) {
+                                continue
+                            }
+
+                            $type = $param.ParameterType.Name
+
+                            $default = $null
+                            if ($null -ne $paramDefs) {
+                                $default = ($paramDefs | Where-Object { $_.DefaultValue -and $_.Name.Extent.Text -ieq "`$$($param.Name)" }).DefaultValue.Value
+                            }
+
+                            if ($type -iin @('boolean', 'switchparameter')) {
+                                New-PodeWebCheckbox -Name $param.Name -AsSwitch
+                            }
+                            else {
+                                switch ($type) {
+                                    'pscredential' {
+                                        New-PodeWebCredential -Name $param.Name
+                                    }
+
+                                    default {
+                                        $multiple = $param.ParameterType.Name.EndsWith('[]')
+
+                                        if ($param.Attributes.TypeId.Name -icontains 'ValidateSetAttribute') {
+                                            $values = ($param.Attributes | Where-Object { $_.TypeId.Name -ieq 'ValidateSetAttribute' }).ValidValues
+                                            New-PodeWebSelect -Name $param.Name -Options $values -SelectedValue $default -Multiple:$multiple
+                                        }
+                                        elseif ($param.ParameterType.BaseType.Name -ieq 'enum') {
+                                            $values = [enum]::GetValues($param.ParameterType)
+                                            New-PodeWebSelect -Name $param.Name -Options $values -SelectedValue $default -Multiple:$multiple
+                                        }
+                                        else {
+                                            New-PodeWebTextbox -Name $param.Name -Value $default
+                                        }
+                                    }
+                                }
+                            }
+                        })
+
+                    $elements += (New-PodeWebHidden -Name '_Function_Name_' -Value $cmd)
+
+                    $name = $set.Name
+                    if ([string]::IsNullOrWhiteSpace($name) -or ($set.Name -iin @('__AllParameterSets'))) {
+                        $name = 'Default'
+                    }
+
+                    $formId = "form_param_$($cmd)_$($name)"
+
+                    $form = New-PodeWebForm -Name Parameters -Id $formId -Content $elements -AsCard -NoAuthentication:$NoAuthentication -ScriptBlock {
+                        $cmd = $WebEvent.Data['_Function_Name_']
+                        $WebEvent.Data.Remove('_Function_Name_')
+
+                        $_args = @{}
+                        foreach ($key in $WebEvent.Data.Keys) {
+                            if ($key -imatch '(?<name>.+)_(Username|Password)$') {
+                                $name = $Matches['name']
+                                $uKey = "$($name)_Username"
+                                $pKey = "$($name)_Password"
+
+                                if (![string]::IsNullOrWhiteSpace($WebEvent.Data[$uKey]) -and ![string]::IsNullOrWhiteSpace($WebEvent.Data[$pKey])) {
+                                    $creds = (New-Object System.Management.Automation.PSCredential -ArgumentList $WebEvent.Data[$uKey], (ConvertTo-SecureString -AsPlainText $WebEvent.Data[$pKey] -Force))
+                                    $_args[$name] = $creds
+                                }
+                            }
+                            else {
+                                if ($WebEvent.Data[$key] -iin @('true', 'false')) {
+                                    $_args[$key] = ($WebEvent.Data[$key] -ieq 'true')
+                                }
+                                else {
+                                    if ($WebEvent.Data[$key].Contains(',')) {
+                                        $_args[$key] = ($WebEvent.Data[$key] -isplit ',' | ForEach-Object { $_.Trim() })
+                                    }
+                                    else {
+                                        $_args[$key] = $WebEvent.Data[$key]
+                                    }
+                                }
+                            }
+                        }
+
+                        try {
+                    (. $cmd @_args) | ConvertTo-Json | Out-PodeWebTextbox -Multiline -Preformat
+                        }
+                        catch {
+                            $_.Exception | ConvertTo-Json -Depth 0 | Out-PodeWebTextbox -Multiline -Preformat
+                        }
+                    }
+
+                    New-PodeWebTab -Name $name -Layouts $form
+                })
+
+            $group = [string]::Empty
+            if ($GroupVerbs) {
+                $group = $cmdInfo.Verb
+                if ([string]::IsNullOrWhiteSpace($group)) {
+                    $group = '_'
+                }
+            }
+
+            Add-PodeWebPage -Name $cmd -Icon Settings -Layouts $tabs -Group $group -NoAuthentication:$NoAuthentication
+        }
+    }
+
     # Avoid the ugly logic of (-not () -or -not ())
     if ((Get-Module -list Pode) -and (Get-Module -list Pode.Web)) {}
     else {
